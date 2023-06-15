dd6dccc693494275c8dc4e892750142572005f7b
Clarisse <78051319+Madlys@users.noreply.github.com>
2023-06-15 15:15:33 +0200

    response codes verification

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -68,7 +68,7 @@ exports.getOneSauce = (req, res, next) => {
 exports.getAllSauces = (req, res, next) => {
     Sauces.find()
         .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(404).json({ error }));
 }
 
 exports.likeSauce = (req, res, next) => {
